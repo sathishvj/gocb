3dce6fc29e1ad7e07c7d3abb4b6eae67ad78b499
sathishvj <sathishvj@yahoo.com>
2013-10-13 20:26:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+gocb
+===
+
+## Build (and run) a go file when it changes
+Repeatedly doing "go build" or "go run" on a file is tedious.  gocb continuosly watches for changes to a go file, and if it has changed, it automatically runs a "go build" on it.  If you have given the -r option, it will also do a "go run" if the build was successful.
+
+The best way to use it is to have a second terminal window/pane where gocb is running.  In your editing window, as soon as you save changes, you will see the build result in the gocb window.
+
+## Installation
+```
+go get github.com/sathishvj/gocb
+```
+
+> This will pull down the source and install the gor command in $GOPATH/bin
+
+## Running gor
+Make sure that your PATH contains $GOPATH/bin.  Then use gocb from any directory.
+
+```
+gocb -r hello.go 
+```
+> this will watch hello.go for changes and run a build if there are any.  Since the -r option is present, it will also run the file if the build is successful.
+
+
+## Help
+```
+gor -h
+```
+
+## Testing
+Checked this only on Mac OS X and with single files. If you find any bugs, please raise an issue for this project.  Thank you.
+
+## ToDo
+* Watch directories for changes.
+* Add option to change poll interval.
+* Add option to do test if build is successful.
+* Add option to set a specific go binary.
+* In execution mode, add option to quit when q is pressed, toggle run when r is pressed, toggle test when t is pressed, increase/decrease time interval when +/- is pressed.
---
+++ gocb.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	help := flag.Bool("h", false, "to display this usage listing")
+	run := flag.Bool("r", false, "run the file once if build was ok")
+	flag.Parse()
+	//anyFlags := help
+
+	if *help {
+		usage()
+		return
+	}
+
+	if len(flag.Args()) == 0 {
+		fmt.Println("Error! No arguments given.\n")
+		usage()
+		return
+	}
+
+	watchFile := flag.Args()[0]
+	fmt.Println("Watching file:", watchFile)
+
+	var prevMod time.Time
+	for {
+		fi, err := os.Stat(watchFile)
+		if err != nil {
+			fmt.Println("Error checking file: ", err)
+			return
+		}
+		modTime := fi.ModTime()
+		if modTime.Sub(prevMod) > 0 {
+			fmt.Println(">> File change detected: ", watchFile)
+			fmt.Println(">> starting build")
+			prevMod = modTime
+			buildErr := exe(watchFile, "build")
+			if !buildErr && *run {
+				fmt.Println(">> build ok. run starting")
+				exe(watchFile, "run")
+				fmt.Println(">> run finished")
+			} else if buildErr {
+				fmt.Println(">> build finished with errors")
+			} else {
+				fmt.Println(">> build ok")
+			}
+		}
+		time.Sleep(time.Second)
+	}
+
+}
+
+//returns true if there was an output on std error
+func exe(watchFile string, tool string) bool {
+
+	cmd := exec.Command("go", tool, watchFile)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error getting StdoutPipe")
+		panic(err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("Error getting StdErrPipe")
+		panic(err)
+	}
+
+	outPipe := bufio.NewReader(stdout)
+	errPipe := bufio.NewReader(stderr)
+	outCh := make(chan string)
+	errCh := make(chan string)
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting cmd")
+		panic(err)
+	}
+	go getStdOutput(outCh, outPipe)
+	go getStdOutput(errCh, errPipe)
+	hasErrOutput := false
+
+outside:
+	for {
+		select {
+		case s, ok := <-outCh:
+			if !ok {
+				break outside
+			}
+			//fmt.Println("From Out:", s)
+			fmt.Println(s)
+		case s, ok := <-errCh:
+			if !ok {
+				break outside
+			}
+			//fmt.Println("Err!", s)
+			fmt.Println(s)
+
+			// This should be a compile error.  (I think.)
+			hasErrOutput = true
+		}
+	}
+	//fmt.Println("Finished")
+
+	err = cmd.Wait()
+	if err != nil {
+		// Typically a compile error. Don't print any message.
+		//fmt.Println("Waiting error", err)
+		return hasErrOutput
+	}
+	return hasErrOutput
+}
+
+func getStdOutput(c chan string, p *bufio.Reader) {
+	for {
+		line, _, err := p.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("ReadLine error:", err)
+				//return
+			}
+			break
+		}
+		//fmt.Println("Got line:", string(line))
+		c <- string(line)
+	}
+	//fmt.Println("Closing c")
+	close(c)
+}
+
+func usage() {
+	pgm := os.Args[0]
+	fmt.Printf("%s will watch for the file change and build it.  Usage:\n", pgm)
+	flag.PrintDefaults()
+	fmt.Printf("\n%s hello.go            \n", pgm)
+	fmt.Printf("%s -r hello.go         #Also run the file on successful build.\n", pgm)
+	return
+}
+
+func write(m map[string]map[string]string, f string) error {
+	var s string
+	for k, v := range m {
+		for k1, v1 := range v {
+			s = s + k + "=" + k1 + "=" + v1 + "\n"
+		}
+	}
+	err := ioutil.WriteFile(f, []byte(s), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
